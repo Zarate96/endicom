b6fffaae1452fbfe3200f4cd79f2590d0224047f
Your Name <you@example.com>
2021-10-20 00:11:26 -0500

    updated fronted

--- pages/admin.py
+++ pages/admin.py
@@ -13,7 +13,7 @@ admin.site.register(Proyectos)
 admin.site.register(Newsletter)
 
 class MensajesProyectotAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nombre', 'email', 'telefono', 'asunto', 'is_answered')
+    list_display = ('id', 'nombre', 'email', 'telefono', 'is_answered')
     search_fields = ('nombre', 'email')
     list_per_page = 10
 
